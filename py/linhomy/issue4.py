e1cbddaf2c22047550a2ad196e33e39d5fddb1a8
Jonathan Fine <jfine@pytex.org>
2014-11-30 17:05:48 +0000

    For #5 refactor doit_C to use generic function non_zero_entries(matrix).

--- py/linhomy/issue4.py
+++ py/linhomy/issue4.py
@@ -12,21 +12,37 @@ def wibble(s):
     return Word(s).worm.shape.arg
 
 
-def doit_C(n):
-    print('C in the g basis.')
-    m = n + 1
+def non_zero_entries(matrix):
 
-    for j in range(FIB[n+1]):
-        for i in range(FIB[m+1]):
+    n_fib, m_fib = matrix.shape
+    if n_fib == 1 or m_fib == 1:
+        raise ValueError
 
-            coeff = C_G[m][i, j]
+    tmp = [FIB[0]]
+    while tmp[-1] < max(n_fib, m_fib):
+        tmp.append(FIB[len(tmp)])
+
+    n = tmp.index(n_fib) - 1
+    m = tmp.index(m_fib) - 1
+
+    for j in range(m_fib):
+        for i in range(n_fib):
+            coeff = matrix[i, j]
             if coeff:
+                i_word = FIBWORDS[n][i]
+                j_word = FIBWORDS[m][j]
+                yield j_word, i_word, coeff
 
-                i_str = wibble(FIBWORDS[m][i])
-                j_str = wibble(FIBWORDS[n][j])
 
-                coeff = '{0:+}'.format(coeff)
-                print(coeff, j_str, i_str)
+def doit_C(n):
+
+    for j_word, i_word, coeff in non_zero_entries(C_G[n]):
+
+        i_str = wibble(i_word)
+        j_str = wibble(j_word)
+
+        coeff = '{0:+}'.format(coeff)
+        print(coeff, j_str, i_str)
 
     print()
 
@@ -51,8 +67,9 @@ def doit_D(n):
 
 if __name__ == '__main__':
 
-    for i in range(N-1):
+    for i in range(2, N-1):
+        print('C in the g basis.')
         doit_C(i)
 
-    for n in range(0, N-2):
+    for n in range(2, N-2):
         doit_D(n)
