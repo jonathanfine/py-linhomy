7bc2a04f4a1ac32357bab57b71160d883d5d3517
Jonathan Fine <jfine@pytex.org>
2014-11-30 19:13:54 +0000

    For #5 move print_entries and non_zero_entries to issue4.py.

--- py/linhomy/issue4.py
+++ py/linhomy/issue4.py
@@ -1,51 +1,10 @@
-from .constants import FIB
-from .constants import FIBWORDS
-import numpy as np
-from .fibsubset import Word
-
-from .issue4tools import CD_G, G_CD, C_CD, C_G, D_CD, D_G
-
-N = 6                           # Was 12.
-
-def wibble(s):
-
-    return Word(s).worm.shape.arg
-
-
-def non_zero_entries(matrix):
-
-    n_fib, m_fib = matrix.shape
-    if n_fib == 1 or m_fib == 1:
-        raise ValueError
-
-    tmp = [FIB[0]]
-    while tmp[-1] < max(n_fib, m_fib):
-        tmp.append(FIB[len(tmp)])
-
-    n = tmp.index(n_fib) - 1
-    m = tmp.index(m_fib) - 1
-
-    for j in range(m_fib):
-        for i in range(n_fib):
-            coeff = matrix[i, j]
-            if coeff:
-                i_word = FIBWORDS[n][i]
-                j_word = FIBWORDS[m][j]
-                yield j_word, i_word, coeff
-
-
-def print_entries(entries):
-
-    for j_word, i_word, coeff in entries:
-
-        i_str = wibble(i_word)
-        j_str = wibble(j_word)
-
-        coeff = '{0:+}'.format(coeff)
-        print(coeff, j_str, i_str)
-
+from .issue4tools import non_zero_entries
+from .issue4tools import print_entries
+from .issue4tools import C_G, D_G
 
 if __name__ == '__main__':
+
+    N = 6                           # Was 12.
 
     for n in range(2, N-1):
 
--- py/linhomy/issue4tools.py
+++ py/linhomy/issue4tools.py
@@ -114,3 +114,44 @@ def _D_G(cache, n):
     return curr
 
 D_G = _D_G._cache
+
+
+# TODO: Rename and test.
+def wibble(s):
+
+    return Word(s).worm.shape.arg
+
+
+# TODO: Test.
+def non_zero_entries(matrix):
+
+    n_fib, m_fib = matrix.shape
+    if n_fib == 1 or m_fib == 1:
+        raise ValueError
+
+    tmp = [FIB[0]]
+    while tmp[-1] < max(n_fib, m_fib):
+        tmp.append(FIB[len(tmp)])
+
+    n = tmp.index(n_fib) - 1
+    m = tmp.index(m_fib) - 1
+
+    for j in range(m_fib):
+        for i in range(n_fib):
+            coeff = matrix[i, j]
+            if coeff:
+                i_word = FIBWORDS[n][i]
+                j_word = FIBWORDS[m][j]
+                yield j_word, i_word, coeff
+
+
+# TODO: Test - but printing to stdout is yuck.
+def print_entries(entries):
+
+    for j_word, i_word, coeff in entries:
+
+        i_str = wibble(i_word)
+        j_str = wibble(j_word)
+
+        coeff = '{0:+}'.format(coeff)
+        print(coeff, j_str, i_str)
