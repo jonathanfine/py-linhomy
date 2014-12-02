e0b82a90ef3f9a65c68e2fe7b3e75d5f0e1d28a9
Jonathan Fine <jfine@pytex.org>
2014-12-02 18:32:38 +0000

    For #6 add c_rule, with tests (again records differences from C_G, previous wrong rule.

--- py/alib_tests/issue6.py
+++ py/alib_tests/issue6.py
@@ -1,5 +1,7 @@
 import numpy as np
+from linhomy.issue6 import c_rule
 from linhomy.issue6 import d_rule
+from linhomy.issue4tools import C_G
 from linhomy.issue4tools import D_G
 
 
@@ -24,4 +26,29 @@ do_d_rule(6) == [(3, 10), (24, 10)]
 do_d_rule(7) == [
     (3, 10), (3, 15), (5, 16), (24, 10),
     (24, 15), (37, 15), (39, 16)
+]
+
+
+def do_c_rule(n):
+
+    aaa = c_rule[n]
+    bbb = C_G[n]
+    diff = np.nonzero(aaa - bbb)
+
+    indices = list(zip(*diff))
+
+    # TODO: Display differences more usefully.
+    return indices
+
+
+do_c_rule(0) == []
+do_c_rule(1) == []
+do_c_rule(2) == []
+do_c_rule(3) == []
+do_c_rule(4) == []
+do_c_rule(5) == [(3, 6), (6, 6)]
+do_c_rule(6) == [(3, 6), (3, 10), (6, 6)]
+do_c_rule(7) == [
+    (3, 6), (3, 10), (5, 17), (6, 6),
+    (9, 17), (11, 19), (17, 17), (19, 19)
 ]
--- py/linhomy/issue6.py
+++ py/linhomy/issue6.py
@@ -46,3 +46,55 @@ def _d_rule(cache, n):
 
 
 d_rule = _d_rule._cache
+
+
+def c_rule_1(v):
+
+    # Might not be any C in v.
+    # Might like to write: if C not in v:
+    if not b'\x01' in v:
+        # GOTCHA: Wrote return value;
+        yield b'\x01' + v      # Prefix with a C.
+        raise StopIteration    # Could I use return instead?
+
+    # Split at last C.
+    left, right = v.rsplit(b'\x01', 1)
+
+    # Join, inserting a C.
+    yield left + b'\x01\x01' + right
+
+
+def c_rule_2(v):
+
+    if not v.endswith(b'\x02'):
+        raise StopIteration
+
+    # Split so tail is trailing D's.
+    index = v.rfind(b'\x01') + 1
+    base, d_tail = v[:index], v[index:]
+
+    length = len(d_tail)
+    for i in range(length, 0, -1):
+
+        d_suffix = b'\x02' * i
+        c_suffix = b'\x01\x01' * (length - i)
+        yield base + c_suffix + d_suffix + b'\x01'
+
+
+@cache_function
+def _c_rule(cache, n):
+
+    value = fib_zeros_array(n+1, n)
+
+    for v in FIBWORDS[n]:
+
+        for w in c_rule_1(v):
+            value[fib_index(w), fib_index(v)] += 1
+
+        for w in c_rule_2(v):
+            value[fib_index(w), fib_index(v)] += 1
+
+    return value
+
+
+c_rule = _c_rule._cache
