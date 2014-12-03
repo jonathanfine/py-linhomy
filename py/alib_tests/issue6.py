469b0cb474697ab073486449b39a87fd5f39e779
Jonathan Fine <jfine@pytex.org>
2014-12-03 20:16:26 +0000

    For #6 show that computed g_from_cd is OK up to n=7, has 1 error at n = 8 and 6 at n = 9.

--- py/alib_tests/issue6.py
+++ py/alib_tests/issue6.py
@@ -1,6 +1,7 @@
 import numpy as np
 from linhomy.issue6 import c_rule
 from linhomy.issue6 import d_rule
+from linhomy.issue4tools import g_from_cd_rules_factory
 from linhomy.issue4tools import C_G
 from linhomy.issue4tools import D_G
 
@@ -52,3 +53,54 @@ do_c_rule(7) == [
     (3, 6), (3, 10), (5, 17), (6, 6),
     (9, 17), (11, 19), (17, 17), (19, 19)
 ]
+
+
+g_from_cd = g_from_cd_rules_factory(c_rule, d_rule)
+
+def do_g_from_cd(n):
+    '''Check that computed g_from_cd is all ones.
+    '''
+
+    matrix = g_from_cd(n)
+
+    r, s = matrix.shape
+    count = 0
+    errors = 0
+
+    for i in range(r):
+        for j in range(s):
+
+            entry = matrix[i, j]
+            if not(entry == 0 or entry == 1):
+                errors += 1
+
+            if entry == 1:
+                count += 1
+
+    return count, errors
+
+
+# Check that computed g_from_cd is all ones.
+do_g_from_cd(0) == (1, 0)
+do_g_from_cd(1) == (1, 0)
+do_g_from_cd(2) == (2, 0)
+do_g_from_cd(3) == (4, 0)
+do_g_from_cd(4) == (8, 0)
+do_g_from_cd(5) == (18, 0)
+do_g_from_cd(6) == (40, 0)
+do_g_from_cd(7) == (91, 0)
+
+# One failure, here it is.
+do_g_from_cd(8) == (206, 1)
+g_from_cd(8)[3, 27] == 2
+
+
+# Six failures, here they are.
+do_g_from_cd(9) == (464, 6)
+
+g_from_cd(9)[3, 27] == 2
+g_from_cd(9)[3, 40] == 3
+g_from_cd(9)[3, 43] == 2
+g_from_cd(9)[5, 43] == 2
+g_from_cd(9)[5, 44] == 2
+g_from_cd(9)[23, 41] == 2
