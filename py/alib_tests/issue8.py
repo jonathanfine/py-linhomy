441dccc420bb2b8c9dc9774f0bc9434fc1a8acd0
Jonathan Fine <jfine@pytex.org>
2014-12-05 06:59:52 +0000

    For #8 add stub cdrules.py.

---
+++ py/alib_tests/issue8.py
@@ -0,0 +1,12 @@
+from linhomy.cdrules import Index
+
+def do_index(s):
+
+    return []
+
+do_index('') == []
+do_index('00') == []
+do_index('10') == []
+do_index('01') == []
+do_index(':') == []
+do_index('12:34') == []
