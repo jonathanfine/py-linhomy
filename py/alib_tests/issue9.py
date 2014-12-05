61f729339670cbb2cd1bd81b30d250e92a1d7008
Jonathan Fine <jfine@pytex.org>
2014-12-05 17:29:58 +0000

    For #9 add stub test file issue9.py.

---
+++ py/alib_tests/issue9.py
@@ -0,0 +1,19 @@
+from linhomy.cdrules import c_rule
+from linhomy.cdrules import d_rule
+from linhomy.cdrules import cd_trace_factory
+from linhomy.cdrules import detect_collisions
+
+cd_trace = cd_trace_factory(c_rule, d_rule)
+
+def doit(n):
+
+    trace = cd_trace(n)
+    return detect_collisions(trace)
+
+
+doit(4) == {}
+doit(5) == {}
+doit(6) == {}
+doit(7) == {}
+doit(8) == {}
+len(doit(9)) == 2
