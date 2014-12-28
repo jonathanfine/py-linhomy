cd2867691637829ad984e9267152ac0aeea4be3c
Jonathan Fine <jfine@pytex.org>
2014-12-28 14:30:38 +0000

    For #11 add J_from_CD (but not confident value is correct).

--- py/linhomy/matrices.py
+++ py/linhomy/matrices.py
@@ -88,3 +88,36 @@ J_from_IC = dict(
     for n in range(11)
     for m in range(11 - n - 1)
 )
+
+
+def _J_from_CD(n, m):
+
+    value = fib_zeros_array(n, m, n + m + 1)
+    matrix = J_from_IC[n, m]
+    rows = numpy.reshape(matrix, (FIB[n+1] * FIB[m+1], -1))
+
+    for i, v in enumerate(FIBWORDS[n]):
+        for j, w in enumerate(FIBWORDS[m]):
+
+            coefficients = [
+                r * s
+                for r in IC_from_CD[n][i]
+                for s in IC_from_CD[m][j]
+            ]
+
+            join_ic = sum(
+                c * r
+                for (c, r) in zip(coefficients, rows)
+            )
+
+            join_cd = numpy.dot(CD_from_IC[n+m+1], join_ic)
+            value[i, j, :] = join_cd
+
+    return value
+
+
+J_from_CD = dict(
+    ((n, m),  _J_from_CD(n, m))
+    for n in range(11)
+    for m in range(11 - n - 1)
+)
