63d493b9ce9fde5af01e49499760bd1061864d38
Jonathan Fine <jfine@pytex.org>
2014-12-31 10:05:10 +0000

    For #14 move J_from_CD out of code file (and into test).  This makes development easier.

--- py/alib_tests/bilinear.py
+++ py/alib_tests/bilinear.py
@@ -1,7 +1,26 @@
 import numpy
-from linhomy.bilinear import J_from_CD
+from linhomy.bilinear import join_factory
 from linhomy.matrices import _cache
 from linhomy.matrices import CD_from_F
+from linhomy.matrices import CD_from_IC
+from linhomy.matrices import IC_from_CD
+from linhomy.matrices import J_from_IC
+
+
+def _J_from_CD(n, m):
+
+    return join_factory(
+        J_from_IC[n, m],
+        IC_from_CD[n],
+        IC_from_CD[m],
+        CD_from_IC[n + m + 1]
+    )
+
+J_from_CD = dict(
+    ((n, m),  _J_from_CD(n, m))
+    for n in range(11)
+    for m in range(11 - n - 1)
+)
 
 def lists_from_matrix(matrix):
 
--- py/linhomy/bilinear.py
+++ py/linhomy/bilinear.py
@@ -1,11 +1,4 @@
 import numpy
-from .constants import FIB
-from .constants import FIBWORDS
-from .issue4tools import fib_zeros_array
-from .matrices import CD_from_IC
-from .matrices import IC_from_CD
-from .matrices import J_from_IC
-
 
 def join_factory(cube, a, b, c):
 
@@ -33,20 +26,3 @@ def join_factory(cube, a, b, c):
             value[i, j, :] = join_cd
 
     return value
-
-
-def _J_from_CD(n, m):
-
-    return join_factory(
-        J_from_IC[n, m],
-        IC_from_CD[n],
-        IC_from_CD[m],
-        CD_from_IC[n + m + 1]
-    )
-
-
-J_from_CD = dict(
-    ((n, m),  _J_from_CD(n, m))
-    for n in range(11)
-    for m in range(11 - n - 1)
-)
