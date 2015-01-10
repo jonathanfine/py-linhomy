48c33a9fcbb20094409267fbffbceb20af180fd7
Jonathan Fine <jfine@pytex.org>
2015-01-10 16:51:34 +0000

    For #22 add is_fibword and is_index.

--- py/alib_tests/issue22.py
+++ py/alib_tests/issue22.py
@@ -1,3 +1,25 @@
+from linhomy.issue22 import is_fibword
+
+is_fibword('') == False
+is_fibword(b'') == True
+is_fibword(b'\x01') == True
+is_fibword(b'\x02') == True
+is_fibword(b'\x03') == False
+
+
+from linhomy.issue22 import is_index
+
+is_index('') == False
+is_index(b'') == False
+is_index(b'\x00') == False
+is_index(b'\x00\x00') == True
+is_index(b'\x00\x05') == True
+is_index(b'\x06\x00') == True
+is_index(b'\x07\x08') == True
+is_index(b'\x07\x08' * 5) == True
+is_index(b'\x07\x08\x09' * 5) == False
+
+
 from linhomy.issue22 import _split
 
 def do_split(s):
--- py/linhomy/issue22.py
+++ py/linhomy/issue22.py
@@ -5,6 +5,27 @@
 # i, j: indices.
 # s, t: loop iteration.
 
+
+def is_fibword(b):
+    '''Return True if type(b) is bytes, all in 1 and 2.
+    '''
+    return (
+        type(b) == bytes
+        # GOTCHA: If b is bytes then set(b) is ints.
+        and set(b).issubset({1, 2})
+    )
+
+
+def is_index(b):
+    '''Return True if type(b) is bytes, length in 2, 4, 6, 8, etc.
+    '''
+    return (
+        type(b) == bytes
+        and not len(b) % 2
+        and len(b) >= 2
+    )
+
+
 def _split(i):
 
     n = len(i)
