f5d9bc4f896e29168d772532848380a46d0dcf8b
Jonathan Fine <jfine@pytex.org>
2015-01-24 10:22:46 +0000

    For #26 add test code do_simple_remove and single test.  Next, refactor test code.

--- py/alib_tests/issue26.py
+++ py/alib_tests/issue26.py
@@ -23,3 +23,32 @@ list(compose_3(3)) == [
     (2, 0, 1), (2, 1, 0),
     (3, 0, 0),
 ]
+
+
+from linhomy.issue26 import simple_remove
+from linhomy.issue26 import slide_remove
+from copy import deepcopy
+from itertools import count
+
+
+def do_simple_remove(s, index, n):
+
+    curr = []
+    for bit in s.split():
+        a, b = bit
+        pair = [int(a), int(b)]
+        curr.append(pair)
+
+    prev = deepcopy(curr)
+    simple_remove(curr, index, n)
+
+    return [
+        (i, prev_pair, curr_pair)
+        for i, prev_pair, curr_pair in zip(count(), prev, curr)
+        if prev_pair != curr_pair
+        ]
+
+
+do_simple_remove('10', 0, 1) == [
+    (0, [1, 0], [0, 2])
+]
