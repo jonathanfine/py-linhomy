62cce07a42693aecbeb9333a136d2f01735a972a
Jonathan Fine <jfine@pytex.org>
2015-01-25 17:20:03 +0000

    For #27 add g_from_CD_1 - there's something wrong here.

--- py/alib_tests/issue27.py
+++ py/alib_tests/issue27.py
@@ -1,4 +1,27 @@
+from collections import Counter
+import numpy
 from linhomy.matrices import G_matrices
-from linhomy.issue27 import g_from_CD
 
-g_matrices = G_matrices(g_from_CD)
+
+def P_stats(matrices, n):
+
+    counter = Counter()
+    for i in range(n+1):
+        j = n - i
+        if i <= j:
+            mat = matrices.P_from_g[i, j]
+            vec = numpy.reshape(mat, [-1])
+            counter.update(vec)
+
+    return sorted(counter.items())
+
+
+from linhomy.issue27 import g_from_CD_1
+g_matrices_1 = G_matrices(g_from_CD_1)
+
+# There's something wrong here.
+P_stats(g_matrices_1, 2) == [(0, 2), (1, 4)]
+P_stats(g_matrices_1, 3) == [(0, 7), (1, 8)]
+P_stats(g_matrices_1, 4) == [(0, 35), (1, 25)]
+P_stats(g_matrices_1, 5) == [(-1, 4), (0, 101), (1, 45), (2, 2)]
+P_stats(g_matrices_1, 6) == [(-2, 2), (-1, 20), (0, 372), (1, 115), (2, 11)]
--- py/linhomy/issue27.py
+++ py/linhomy/issue27.py
@@ -1,6 +1,9 @@
+import itertools
 from .cdrules import index_from_fibword
+from .issue26 import iter_contribute
 
-def g_from_CD(fibword):
+
+def g_from_CD_0(fibword):
 
     # According to matrices.py, _g_from_CD passes a fibword to
     # supplied g_from_CD (as coded here), and expects an iterable of
@@ -10,3 +13,21 @@ def g_from_CD(fibword):
     #   return [b'' + index_from_fibword(fibword)]
 
     return [b'' + index_from_fibword(fibword)]
+
+
+def g_from_CD_1(fibword):
+
+    # Convert fibword in pairs, list of pairs of integers.
+    index = index_from_fibword(fibword)
+    iter_index = iter(index)
+    pairs = list(zip(iter_index, iter_index))
+
+    # Contributions is iterable of pairs of integers.
+    contributions = iter_contribute(pairs)
+
+    value = []
+    for contrib in contributions:
+        b = bytes(itertools.chain(*contrib))
+        value.append(b)
+
+    return value
