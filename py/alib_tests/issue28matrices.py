49093b3b95cf52b4c8d4494a54d9665538525590
Jonathan Fine <jfine@pytex.org>
2015-01-31 15:04:04 +0000

    For #28 start to understand the n = 8 negative values.

--- py/alib_tests/issue28matrices.py
+++ py/alib_tests/issue28matrices.py
@@ -3,7 +3,8 @@ from linhomy.testtools import P_stats
 from linhomy.issue27 import g_from_CD_factory
 from linhomy.issue28 import iter_contribute
 
-g_matrices_1 = G_matrices(g_from_CD_factory(iter_contribute)[1])
+g_from_CD_matrix, g_from_CD = g_from_CD_factory(iter_contribute)
+g_matrices_1 = G_matrices(g_from_CD)
 
 # These are pretty much as expected.  No negatives.
 P_stats(g_matrices_1, 2) == [(0, 2), (1, 4)]
@@ -18,13 +19,13 @@ P_stats(g_matrices_1, 7) == [(0, 1222), (1, 141), (2, 2)]
 # n = 8. Two negatives - I wonder where and why.
 P_stats(g_matrices_1, 8) == [(-1, 2), (0, 4058), (1, 349), (2, 11)]
 
-# n = 9. Now have 11 negatives.
+# n = 9. Now have 18 negatives.
 P_stats(g_matrices_1, 9) == [
     (-2, 1), (-1, 10),
     (0, 10884), (1, 630), (2, 24), (3, 1),
 ]
 
-# n = 10. Now have 90 negatives.
+# n = 10. Now have 113 negatives.
 P_stats(g_matrices_1, 10) == [
     (-2, 12), (-1, 78),
     (0, 34266), (1, 1506), (2, 90), (3, 4),
@@ -49,11 +50,34 @@ str_from_word(3, 1) == '12'     # '00:00'
 str_from_word(5, 4) == '122'    # '00:10'
 str_from_word(5, 6) == '212'    # '10:00'
 
-# 8 = 4 + 4.
-str_from_word(4, 1) == '112'    # '01:00'
-
-
 # Here are the exceptions. They are the order 2 words that end with a
 # '1'.
+
 str_from_word(8, 15) == '121121' # '00:01:01' gives -1.
 str_from_word(8, 16) == '121211' # '00:00:02' gives -1.
+
+
+# Start to understand the negative values.
+
+#   Let's unpack (3, 5, 1, 4, 16, -1).
+list(g_matrices_1.CD_P_g[3, 5][1, 4]) == [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1,
+    1, 1, 1, -1, 0, 1, 0, 1, -1, 0, 0, 0, -1, -1, 0, 1, 0,
+]
+
+list(g_matrices_1.g_from_CD[8][16,:]) == [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+
+# Let's unpack (3, 5, 1, 6, 15, -1).
+list(g_matrices_1.CD_P_g[3, 5][1, 6]) == [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 1, 0, 0, 1, 0, 1,
+    0, -1, 0, 0, 0, 0, 1, -1, 0, 0, 2, -1, 0, -2, 0, 1, 0
+]
+
+list(g_matrices_1.g_from_CD[8][15,:]) == [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
